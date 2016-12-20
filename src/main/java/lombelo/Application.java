c58efbf949d06a9cf28039adb5d6cf96ce1cd07c
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-20 18:32:40 +0100

    Add default Application

---
+++ src/main/java/lombelo/Application.java
@@ -0,0 +1,16 @@
+package lombelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Niklas Wünsche
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
