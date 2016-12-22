0052725db420aac5edff4f6e6a790877763a48f5
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-22 17:03:20 +0100

    Add model for content of a note

---
+++ src/main/java/lombelo/model/ContentOfNote.java
@@ -0,0 +1,12 @@
+package lombelo.model;
+
+import lombok.Data;
+
+/**
+ * @author Niklas Wünsche
+ */
+@Data
+public class ContentOfNote {
+    private String titleOfNote;
+    private String textOfNote;
+}
