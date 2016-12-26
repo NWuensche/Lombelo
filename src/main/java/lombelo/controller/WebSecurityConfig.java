f3cc30cedab6fa4b83cf233d23b353e8654e29f2
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-26 07:11:12 +0100

    Fix SS with css

--- src/main/java/lombelo/controller/WebSecurityConfig.java
+++ src/main/java/lombelo/controller/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/resources/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
