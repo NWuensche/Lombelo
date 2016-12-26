4b921f6f9b5d8e2d268cfcd0e20600701a4e1f64
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-26 08:07:35 +0100

    disable csrf

--- src/main/java/lombelo/controller/WebSecurityConfig.java
+++ src/main/java/lombelo/controller/WebSecurityConfig.java
@@ -24,6 +24,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http
                 .authorizeRequests()
                 .antMatchers("/", "/resources/**").permitAll()
