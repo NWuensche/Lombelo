d4c72514b7f6fd498b6e6b7ccdc4bc5784e96496
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2016-12-26 08:26:37 +0100

    Add logout

--- src/main/java/lombelo/controller/WebSecurityConfig.java
+++ src/main/java/lombelo/controller/WebSecurityConfig.java
@@ -36,7 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/");
     }
 
     @Autowired
